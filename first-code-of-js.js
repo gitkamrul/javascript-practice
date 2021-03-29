1fd014ba07eebd9d8c041bd269e1607803849156
kamrulhasan <kamrulhasan00002019@gmail.com>
2021-03-30 03:23:42 +0600

    all change of first code js

---
+++ first-code-of-js.js
@@ -0,0 +1,15 @@
+// string print 
+console.log("Hello World ");
+console.log("I love Bangladesh ");
+
+// number print 
+console.log(45);
+
+// float number print 
+console.log(69.3);
+
+// number substraction 
+console.log(9+9);
+
+// string and number sub 
+console.log("9"+9);
