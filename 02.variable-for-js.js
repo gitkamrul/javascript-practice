7c70ea3c8f23a41813fa583d5baca3945dd9c33b
kamrulhasan <kamrulhasan00002019@gmail.com>
2021-03-30 03:54:52 +0600

    varialbe-for js file create

---
+++ 02.variable-for-js.js
@@ -0,0 +1,22 @@
+
+
+//string  variable declaire for javascript 
+var fname = "kamrul hasan ";
+
+//number   variable declaire for javascript 
+var age = 20;
+
+
+// variable print for javascript 
+console.log(fname + "knows javascript ");
+console.log("His age is only : "+ age);
+console.log(fname + "knows javascript ");
+console.log(fname + "creating javascript tutirial");
+
+
+
+// Error for javascript programming if print after variable declaire
+
+// var fname = "kamrul hasan ";
+// var age = 20;
+
