536947a1af2823c9fcb6a083b8cd1eb4eccab070
kamrulhasan <kamrulhasan00002019@gmail.com>
2021-03-30 14:46:42 +0600

    if else if or else statement and swithc statement create file

---
+++ 10. If-elseif-else-condition.js
@@ -0,0 +1,14 @@
+
+
+// Logic Condition check 
+var a = 10;
+var b = 20;
+
+if(a<b){
+   console.log("b number is big ");
+}
+else if(a<b){
+   console.log("a number is big ");
+}else{
+   "False your Condition"
+}
