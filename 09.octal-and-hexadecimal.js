a67274a191d40199363444564770a973826fa1e4
kamrulhasan <kamrulhasan00002019@gmail.com>
2021-03-30 12:25:35 +0600

    octal and hexadecimal file create for javasript

---
+++ 09.octal-and-hexadecimal.js
@@ -0,0 +1,11 @@
+
+
+// Octal numbers
+var n = 0755; // 493
+var m = 0644; // 420
+
+
+// hexadecimal number 
+0xFFFFFFFFFFFFFFFFF // 295147905179352830000
+0x123456789ABCDEF   // 81985529216486900
+0XA                 // 10
