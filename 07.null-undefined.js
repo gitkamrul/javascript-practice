9789f566ce8e6416175b9ab9c45c39c36b6ce3a8
kamrulhasan <kamrulhasan00002019@gmail.com>
2021-03-30 11:46:00 +0600

    null and undifined file create for javascript

---
+++ 07.null-undefined.js
@@ -0,0 +1,52 @@
+
+// JavaScript Demo: Standard built-in objects - Null
+
+
+function getVowels(str) {
+   const m = str.match(/[aeiou]gi);
+   if (m === null) {
+     return 0;
+   }
+   return m.length;
+ }
+ 
+ console.log(getVowels('sky'));
+ // expected output: 0
+ 
+
+//  Boolean primitives.
+
+var x = false;
+if (x) {
+  // this code is not executed
+}
+
+
+
+// undefined
+
+// JavaScript Demo: Standard built-in objects - undefined
+function test(t) {
+   if (t === undefined) {
+     return 'Undefined value!';
+   }
+   return t;
+ }
+ 
+ let x;
+ 
+ console.log(test(x));
+ // expected output: "Undefined value!"
+
+
+//  Difference of null vs undefined  
+ 
+typeof null          // "object" (not "null" for legacy reasons)
+typeof undefined     // "undefined"
+null === undefined   // false
+null  == undefined   // true
+null === null        // true
+null == null         // true
+!null                // true
+isNaN(1 + null)      // false
+isNaN(1 + undefined) // true
