61b2eb7d78567f59a3ecb0eec365bdeeda0d198f
kamrulhasan <kamrulhasan00002019@gmail.com>
2021-04-01 02:08:31 +0600

    Accessing object again edit

--- 20.Accessing-object.js
+++ 20.Accessing-object.js
@@ -3,9 +3,19 @@
 var obj = {
    a:10,
    b:20,
-   c:30
+   c:30,
+   d:40
 }
 
 //First  Accessing object 
 console.log(obj.a+obj.b);
-//Output: 
+//Output: 30;
+
+// Array Notation Accessign  in object 
+
+console.log("Array Notation Object : " +obj['c']);
+//Output: Array Notation Object : 30
+
+var show = 'd';
+console.log(obj[show]);
+//Output: 40;
