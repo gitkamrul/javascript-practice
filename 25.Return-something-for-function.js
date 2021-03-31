252d9706adb103c0a5c812fbed50b567e7026a96
kamrulhasan <kamrulhasan00002019@gmail.com>
2021-04-01 03:22:32 +0600

    return file something change this file

--- 25.Return-something-for-function.js
+++ 25.Return-something-for-function.js
@@ -5,6 +5,15 @@ function add(a,b){
    return  result;
    
 }
-
 var r = add(10,20);       //Output 30
 console.log(r);
+
+function myFunc(name,email){
+   return {
+      name:name,
+      email:email
+   }
+}
+
+var kamrul = myFunc("kamrul hasan ", "kamrul@gmail.com");
+console.log(kamrul);        //Output { name: 'kamrul hasan ', email: 'kamrul@gmail.com' }
