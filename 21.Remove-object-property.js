0355564381ec0422fcf21d6e9943543e09ca2607
kamrulhasan <kamrulhasan00002019@gmail.com>
2021-04-01 02:20:36 +0600

    remove oject  property

---
+++ 21.Remove-object-property.js
@@ -0,0 +1,13 @@
+
+var obj ={
+   name:"kamrul hasan",
+   district:"sherpur",
+   age:10,
+   thana:"nakla"
+}
+console.log(obj);
+//Output: { name: 'kamrul hasan', district: 'sherpur', age: 10, thana: 'nakla' }
+
+delete obj.thana;
+console.log(obj);
+//Output: { name: 'kamrul hasan', district: 'sherpur', age: 10 }
