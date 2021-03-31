36815057a473b05b79b5bb2284e7eff6bcbe0259
kamrulhasan <kamrulhasan00002019@gmail.com>
2021-04-01 01:52:50 +0600

    Literal construction object file create

---
+++ 19.Literal-constructor-object.js
@@ -0,0 +1,24 @@
+
+// Null Object 
+var obj = {}
+console.log(typeof obj)
+//Output: object
+var obj2 = {
+   fname:"kamrul",
+   lname:"hasan",
+   age:20
+
+}
+
+console.log(obj2);
+//Output : { fname: 'kamrul', lname: 'hasan', age: 20 }
+
+// Update data for object
+obj2.age = 21;
+console.log(obj2)
+//Output : { fname: 'kamrul', lname: 'hasan', age: 21 }
+
+// Add Object  
+obj2.village = "Madirpar";
+console.log(obj2)
+//Output : { fname: 'kamrul', lname: 'hasan', age: 21, village: 'Madirpar' }
