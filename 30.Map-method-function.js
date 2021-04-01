ed945a9d4171cea6a63b23706068d78da7a34ebd
kamrulhasan <kamrulhasan00002019@gmail.com>
2021-04-01 12:04:45 +0600

    map function creat file

---
+++ 30.Map-method-function.js
@@ -0,0 +1,13 @@
+
+// Map function 
+
+var arr = [10,20,30]
+
+var newArr = arr.map(function(value){
+   var result = value*value;
+     return result
+});
+
+
+console.log(newArr);      //Output : [ 100, 400, 900 ]
+
