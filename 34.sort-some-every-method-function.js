0d0c8488a535fc3aa25d8b1394908dcc4d8e37f0
kamrulhasan <kamrulhasan00002019@gmail.com>
2021-04-01 13:17:58 +0600

    sort method function create this file

---
+++ 34.sort-some-every-method-function.js
@@ -0,0 +1,46 @@
+
+
+var nums =  [1, 30,-1, 4, 21, -100 ,100000];
+function sortNumber(a,b){
+   return a - b;
+}
+console.log(nums.sort(sortNumber))
+
+
+
+
+var persons = [
+   {
+      name:"A",
+      age:10
+   },
+   {
+      name:"B",
+      age:10
+   },
+   {
+      name:"C",
+      age:10
+   },
+   {
+      name:"D",
+      age:10
+   }
+]
+
+
+
+
+
+// 34.sort-some-every-method-function.js
+
+
+
+
+
+
+
+
+
+
+//  34.sort-some-every-method-function.js
