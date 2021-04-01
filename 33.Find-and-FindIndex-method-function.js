4312bb41450546c2ea402de661ec1ff7ff240130
kamrulhasan <kamrulhasan00002019@gmail.com>
2021-04-01 12:52:15 +0600

    find  and findindex methnod function file create

---
+++ 33.Find-and-FindIndex-method-function.js
@@ -0,0 +1,21 @@
+
+
+var arr = [10,11,12,13,14,15,16,17,18,19];
+
+// Find Method use function 
+var result = arr.find(function(value){
+   return  value===15
+ });
+ console.log(result);       //Output: 15
+
+//  FindIndex method use function 
+ var result2 = arr.findIndex(function(value){
+   return  value===15
+ });
+ console.log(result2);       //Output: 5
+
+
+
+
+
+//   33.Find-and-FindIndex-method-function.js
